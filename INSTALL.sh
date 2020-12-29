aa4c1e168f26a898f8ad7e7c798470cd2afd2e8a
jontrossbach <jon@trossba.ch>
2020-12-28 23:32:37 -0500

    add install script

---
+++ INSTALL.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+apt install git make podman libfuse3-dev runc
+
+mkdir ~/.config/containers/
+
+me="$(whoami)"
+
+echo "[storage]
+  driver = \"overlay\"
+  runroot = \"/run/user/1000\"
+  graphroot = \"/home/$me/.local/share/containers/storage\"
+  [storage.options]
+    mount_program = \"/usr/bin/fuse-overlayfs\"" >> storage.conf 
+
+rm -rf /home/virty/.local/share/containers/storage
